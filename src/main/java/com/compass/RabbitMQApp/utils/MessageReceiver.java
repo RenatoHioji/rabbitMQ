8d308d41280d842bd37ae43e81572d7948ac141a
RenatoHioji <okamoto.hioji@gmail.com>
2024-09-23 23:20:45 -0300

    receiver: throwing exception with catch

--- src/main/java/com/compass/RabbitMQApp/utils/MessageReceiver.java
+++ src/main/java/com/compass/RabbitMQApp/utils/MessageReceiver.java
@@ -8,15 +8,15 @@ import java.util.Objects;
 @Component
 public class MessageReceiver {
     public void receiveMessage(String message){
-        /*try{*/
+        try{
             System.out.println("Processando mensagem: "+ message);
             if(Objects.equals(message, "error")){
                 throw new RuntimeException("Encaminhando para DLQ");
             }
-            /*
+
         }catch (Exception e){
             System.out.println("Exception here");
-        }*/
+        }
 
     }
 
